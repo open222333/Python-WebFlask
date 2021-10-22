43f17113551f0da2592656e76cf12eab5f1ddbfb
tom_li <tom_li@appdet.com>
2021-10-22 09:39:05 +0800

    -m20211022-093841 - add git_push

---
+++ git_push.sh
@@ -0,0 +1,7 @@
+time=$(date "+%Y%m%d-%H%M%S")
+read -p "commit title:" title
+read -p "commit description:" commit_d
+commit_d="${time} - ${title}"
+git add *
+git commit -m"${commit_t}" -m"${commit_d}"
+git push origin main
