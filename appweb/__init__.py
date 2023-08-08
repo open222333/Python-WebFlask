cc355796cf0f6c38a2443921830d6ef61b4b789e
TomLi <open222333@gmail.com>
2023-08-08 17:32:14 +0800

    修改 依賴模組版本

--- appweb/__init__.py
+++ appweb/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
 from flask_sqlalchemy import SQLAlchemy
+from config.flask_config import config
+from appweb.v01.view import app_v01
 import os
 
 
@@ -21,7 +23,6 @@ def create_app(config_name=None):
         return 'ok'
 
     # 載入設定檔
-    from config.flask_config import config
     if config_name != None:
         app.config.from_project(config[config_name])
 
@@ -33,7 +34,6 @@ def create_app(config_name=None):
         sqlalchemy.init_app(app)
 
     # 每個藍圖可以有自己的 模板 靜態目錄 視圖 URL規則
-    from appweb.v01.view import app_v01
     app.register_blueprint(app_v01, url_prefix='/v01')
 
     return app
